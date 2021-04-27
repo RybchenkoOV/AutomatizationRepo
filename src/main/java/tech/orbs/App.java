e50df659010a68dce30576265fc10c8be88f87b8
ORBS <ribus@mail.ru>
2021-04-27 22:31:54 +0300

    Project with archetype quickstart

---
+++ src/main/java/tech/orbs/App.java
@@ -0,0 +1,13 @@
+package tech.orbs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
